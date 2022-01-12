e4be68900276b5ce20f0ce6a47d3708ee7b74087
JaredHerrera6 <herrerajared13@gmail.com>
2022-01-11 20:57:50 -0500

    editing names

--- app/src/main/java/com/example/materialscalculator/Home.java
+++ app/src/main/java/com/example/materialscalculator/Home.java
@@ -9,21 +9,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        
+        setContentView(R.layout.activity_main);
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Jared's Jobs");
-        actionBar.setSubtitle("Home");
+        actionBar.setSubtitle("Materials Calculator");
         actionBar.setIcon(R.drawable.ic_subpic);
         actionBar.setDisplayUseLogoEnabled(true);
-        //actionBar.setDisplayShowHomeEnabled(true);
-        //actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(true);
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -38,7 +39,7 @@ public class MainActivity2 extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.home:
                 Toast.makeText(this,"Home Clicked", Toast.LENGTH_SHORT).show();
-
+                startActivity(new Intent(Home.this, Home.class));
                 break;
             case R.id.history:
                 Toast.makeText(this,"History Clicked", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/materialscalculator/Settings.java
+++ app/src/main/java/com/example/materialscalculator/Settings.java
@@ -1,32 +1,28 @@
 package com.example.materialscalculator;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.Settings);
+        
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Jared's Jobs");
-        actionBar.setSubtitle("Materials Calculator");
+        actionBar.setSubtitle("Settings");
         actionBar.setIcon(R.drawable.ic_subpic);
         actionBar.setDisplayUseLogoEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
-
+        //actionBar.setDisplayShowHomeEnabled(true);
+        //actionBar.setDisplayHomeAsUpEnabled(false);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -41,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.home:
                 Toast.makeText(this,"Home Clicked", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+
                 break;
             case R.id.history:
                 Toast.makeText(this,"History Clicked", Toast.LENGTH_SHORT).show();
